c7d7272edae59e6d64a51425ddfd4738fcd98118
Tom Mergui <115085780+tomparadise06@users.noreply.github.com>
2025-01-30 18:33:34 +0100

    UPDATE

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { LanguageProvider } from "./context/LanguageContext";
 
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <LanguageProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </LanguageProvider>
   </React.StrictMode>
 );
